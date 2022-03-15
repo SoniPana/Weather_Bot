6a171fd3e06a03497018071ec65bb91ec45b694a
Geusen <89836078+Geusen@users.noreply.github.com>
2022-03-15 20:52:05 +0900

    Update Weather_Bot_am.py

--- Weather_Bot_am.py
+++ Weather_Bot_am.py
@@ -68,7 +68,7 @@ driver.quit()
 
 # 画像トリミング
 im = Image.open('image.png')
-im.crop((20, 100, 850, 480)).save('weather.png', quality=95)
+im.crop((20, 100, 840, 500)).save('weather.png', quality=95)
 #-----------------------------------------------------------------------------
 #画像付きツイート
 api.update_status_with_media(status = 'てすと。 今日、明日の天気です。\n\nFrom 気象庁', filename = 'weather.png')
